88ac28750dc159a69ca350c078cac8acedfe1443
razibraj <razibraj56@gmail.com>
2023-12-02 16:02:20 +0600

    create the details page

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ app.use(cors());
 app.use(express.json());
 
 
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.jgjgvfa.mongodb.net/?retryWrites=true&w=majority`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
@@ -35,6 +35,13 @@ async function run() {
     app.get('/feature', async(req, res) => {
         const result = await featureCollection.find().toArray();
         res.send(result);
+    })
+
+    app.get('/feature/:id', async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) }
+      const result = await featureCollection.findOne(query)
+      res.send(result)
     })
 
     app.get('/trending', async(req, res) => {
